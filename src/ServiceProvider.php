a4a08527f15294c1dcae57c8f37249a38d5a6f27
Michal Nosavcov <mnosavcov@gmail.com>
2018-09-16 16:36:50 +0200

    edit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace C18app\Cmsx;
+
+use Illuminate\Support\ServiceProvider as SP;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Schema;
+
+class ServiceProvider extends SP
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+//        Schema::defaultStringLength(191);
+//        $this->mergeConfigFrom(
+//            __DIR__.'/config/cmsx.php', 'cmsx'
+//        );
+//        $this->loadRoutesFrom(__DIR__.'/routes.php');
+//        $this->loadViewsFrom(__DIR__.'/views/templates/default', 'cmsx-templates-default');
+//        $this->loadViewsFrom(__DIR__.'/views/templates-admin/default', 'cmsx-templates-admin-default');
+//        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+//
+//        // publishes
+//        $this->publishes([
+//            __DIR__.'/assets' => public_path('vendor/c18app/cmsx')
+//        ], 'c18app_cmsx');
+//
+//        $this->publishes([
+//            __DIR__.'/config/cmsx.php' => config_path('cmsx.php')
+//        ], 'c18app_cmsx-config');
+//
+//        $this->publishes([
+//            __DIR__.'/views/templates/default/customizable' => resource_path('views/vendor/cmsx/templates/default/customizable')
+//        ], 'c18app_cmsx-templates-default-custom');
+//
+//        $this->publishes([
+//            __DIR__.'/views/templates/default' => resource_path('views/vendor/cmsx/templates/default')
+//        ], 'c18app_cmsx-templates-default');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,59 @@
+<?php
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('storage/{folder}/{filename}', function ($folder, $filename) {
+        $path = storage_path() . '/app/public/' . $folder . '/' . $filename;
+
+        if (!File::exists($path)) {
+            abort(404);
+        }
+
+        $file = File::get($path);
+        $type = File::mimeType($path);
+
+        $response = Response::make($file, 200);
+        $response->header("Content-Type", $type);
+        return $response;
+    })->name('storage');
+
+    Route::get('login', 'C18app\Cmsx\Controllers\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'C18app\Cmsx\Controllers\Auth\LoginController@login');
+    Route::get('logout', 'C18app\Cmsx\Controllers\Auth\LoginController@logout')->name('logout');
+
+    Route::get('register', 'C18app\Cmsx\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'C18app\Cmsx\Controllers\Auth\RegisterController@register');
+
+    Route::get('password/reset', 'C18app\Cmsx\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'C18app\Cmsx\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'C18app\Cmsx\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'C18app\Cmsx\Controllers\Auth\ResetPasswordController@reset');
+
+    Route::prefix('admin')->group(function() {
+        Route::redirect('/', 'admin/dashboard');
+        Route::get('dashboard', 'C18app\Cmsx\Controllers\Admin\AdminController@dashboard')->name('admin.dashboard');
+
+        Route::resources([
+            'pages' => 'C18app\Cmsx\Controllers\Admin\PageController',
+            'tags' => 'C18app\Cmsx\Controllers\Admin\TagController',
+            'translates' => 'C18app\Cmsx\Controllers\Admin\TranslateController',
+            'articles' => 'C18app\Cmsx\Controllers\Admin\ArticleController'
+        ]);
+
+        Route::post('{type}/sort', 'C18app\Cmsx\Controllers\Admin\AdminController@sort')->name('admin.sort');
+        Route::view('tweet', Config('cmsx.app.template-admin').'::nocontent')->name('admin.tweets');
+        Route::view('content', Config('cmsx.app.template-admin').'::nocontent')->name('admin.content');
+        Route::view('comment', Config('cmsx.app.template-admin').'::nocontent')->name('admin.comment');
+        Route::view('file', Config('cmsx.app.template-admin').'::nocontent')->name('admin.file');
+        Route::view('gallery', Config('cmsx.app.template-admin').'::nocontent')->name('admin.gallery');
+        Route::view('category', Config('cmsx.app.template-admin').'::nocontent')->name('admin.category');
+        Route::view('menu', Config('cmsx.app.template-admin').'::nocontent')->name('admin.menu');
+        Route::view('user', Config('cmsx.app.template-admin').'::nocontent')->name('admin.user');
+        Route::view('maillist', Config('cmsx.app.template-admin').'::nocontent')->name('admin.maillist');
+        Route::view('settings', Config('cmsx.app.template-admin').'::nocontent')->name('admin.setting');
+    });
+
+    Route::get('page/{id}-{slug}', 'C18app\Cmsx\Controllers\CmsController@page')->name('cms.page');
+    Route::get('article/{id}-{slug}', 'C18app\Cmsx\Controllers\CmsController@article')->name('cms.article');
+
+    Route::get('/', 'C18app\Cmsx\Controllers\CmsController@index')->name('homepage');
+});
