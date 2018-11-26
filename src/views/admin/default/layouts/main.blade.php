4ef05d0616719a73850721757e838144fc08b487
Michal Nosavcov <mnosavcov@gmail.com>
2018-11-26 22:41:33 +0100

    ú

--- src/views/admin/default/layouts/main.blade.php
+++ src/views/admin/default/layouts/main.blade.php
@@ -27,6 +27,38 @@
         <link rel="stylesheet" href="/vendor/c18app/cmsx/css/admin/app.css?v={{ config('cmsx.version') }}">
         <script src="/vendor/c18app/cmsx/js/admin/app.js?v={{ config('cmsx.version') }}"></script>
     @endif
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=15atz047gmdimv7gcyfzwslilbkvedor863hcqd46cueig4h"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            theme: 'modern',
+            plugins: 'print preview fullpage paste powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+            toolbar1: 'image | formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+            image_advtab: true,
+            templates: [
+                { title: 'Test template 1', content: 'Test 1' },
+                { title: 'Test template 2', content: 'Test 2' }
+            ],
+            content_css: [
+                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                '//www.tinymce.com/css/codepen.min.css'
+            ],
+            images_upload_url: '/postAcceptor.php',
+            images_upload_base_path: '/images',
+            relative_urls : false,
+            convert_urls : true,
+            init_instance_callback: function (ed) {
+                ed.execCommand('mceImage');
+            }
+        });
+
+        tinymce.activeEditor.uploadImages(function(success) {
+            $.post('ajax/post.php', tinymce.activeEditor.getContent()).done(function() {
+                console.log("Uploaded images and posted content as an ajax request.");
+            });
+        });
+    </script>
 </head>
 <body class="admin">
 @include(Config('cmsx.app.template-admin').'::@topmenu')
