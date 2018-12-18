56aed9bc0925a2b19de95084f75367ebbcc4b70c
Michal Nosavcov <mnosavcov@gmail.com>
2018-12-18 22:02:28 +0100

    auth.logout

--- src/views/admin/default/@topmenu.blade.php
+++ src/views/admin/default/@topmenu.blade.php
@@ -13,7 +13,14 @@
         </ul>
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                <a class="nav-link" href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </li>
         </ul>
     </div>
