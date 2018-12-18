e676decc340edc0ca37c4228e06a29753fde9a5d
Michal Nosavcov <mnosavcov@gmail.com>
2018-12-18 21:21:36 +0100

    auth.reset

--- src/Controllers/Auth/ResetPasswordController.php
+++ src/Controllers/Auth/ResetPasswordController.php
@@ -1,39 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace C18app\Cmsx\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\ResetPasswordController as RPC;
 
-class ResetPasswordController extends Controller
+class ResetPasswordController extends RPC
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+    protected $redirectTo = '/';
 
-    use ResetsPasswords;
-
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function showResetForm(Request $request, $token = null)
     {
-        $this->middleware('guest');
+        return view(Config('cmsx.app.template').'::auth.passwords.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
     }
 }
