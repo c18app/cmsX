3c148ad453c4377a9ce1b5f63ad9e39b8fa261b4
Michal Nosavcov <mnosavcov@gmail.com>
2018-12-22 11:32:10 +0100

    route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::view('/', config('cmsx.app.template').'::cmsx')->name('cmsx');
+Route::view('/', config('cmsx.app.template').'::cmsx')->name('home');
 
 Auth::routes(['verify' => true]);
